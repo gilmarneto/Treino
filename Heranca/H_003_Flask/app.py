1464f433b9c4c2a58590071f0b84f3bb4c5246c0
gilmarneto <jumas.neto@gmail.com>
2024-09-24 20:57:37 -0300

    criação do formulario

--- Heranca/H_003_Flask/app.py
+++ Heranca/H_003_Flask/app.py
@@ -1,15 +1,24 @@
 # data: 24/09/2024
-from flask import Flask, render_template, redirect, url_for
-from class_lista import Lista
+from flask import Flask, render_template, request, redirect, url_for
+from class_lista import Lista 
 
 l = Lista()
 
 app = Flask(__name__)
 @app.route('/')
 def index():
-    l.adicionar()
     return render_template('index.html', titulo='Herança e Flask', componente=l.componentes_tela())
 
-app.run(debug=True)
+@app.route('/adicionar', methods=['POST',])
+def adicionar_letra():
+    letra = request.form['let']
+    print(letra)
+    return redirect(url_for('index'))
+
+@app.route('/editar')
+def editar():
+    return redirect(url_for('index'))
+
+app.run(host='0.0.0.0', port=8888, debug=True)
 
 
